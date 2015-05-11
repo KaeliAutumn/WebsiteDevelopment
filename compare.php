48e88e3f3a204e8303a276d59262c6413da73390
KaeliAutumn <hughes.525@osu.edu>
2015-05-11 15:00:51 -0400

    Update compare.php
    This is my version of compare.php. I found the outline for this online and then changed it to apply to our situation.
    I have comments that look like this: imageName ="revision" + val3.toString() + "_anita" + val1.toString() +"_energy" + val2.toString() + val4 + ".pdf"
    These lines will rewrite the image name to one the depends on the variables in each drop box. They're only commented right now because those files don't exits for me yet- I just used four generic plots to see if the picture would actually change. Does that make sense? 
    So, the next step is to get this to work for the actual files, which shouldn't be too hard. Do we want to deal with PDF files at all? Also I'm still learning how to use this website so if I'm screwing something up please let me know!

--- compare.php
+++ compare.php
@@ -14,13 +14,99 @@
 
 <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
 
-<title>AraSimQC Test Site</title>
+<title>IcemcQC Test Site</title>
+
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <title> - jsFiddle demo</title>
+  
+  <script type='text/javascript' src='jquery-1.10.1.js'></script>
+  
+   
+  <style type='text/css'>
+    img {
+    width:700px;
+    margin:auto;
+   }
+  </style>
+  
+
+
+<script type='text/javascript'>//<![CDATA[ 
+$(window).load(function(){
+var pictureList = [
+    "plots/primaryplots/altitude_int.jpg",
+    "plots/primaryplots/currentint.jpg",
+    "plots/primaryplots/nuflavorint.jpg",
+    "plots/primaryplots/nuexit.jpg"];
+
+
+
+$('#anita').change(function () {
+//    var val1 = parseInt($('#anita').val());
+//    console.log(val1);
+//    console.log(pictureList[val1]);
+//    $('img').attr("src",pictureList[val1]);
+    var val1 = parseInt($('#anita').val());
+    var val2 = parseInt($('#energy').val());
+    var val3 = parseInt($('#revision').val());
+    var val4 = $('#plot').val();
+    //imageName = "anita" + val1.toString() + "_energy" + val2.toString() + ".jpg"
+	imageName = "plots/primaryplots/altitude_int.jpg";
+    console.log(val1);
+    console.log(imageName);
+    $('img').attr("img",imageName);
+});
+
+
+$('#energy').change(function () {
+    var val1 = parseInt($('#anita').val());
+    var val2 = parseInt($('#energy').val());
+    var val3 = parseInt($('#revision').val());
+    var val4 = $('#plot').val();
+    //imageName = "anita" + val1.toString() +"_energy" + val2.toString() + ".pdf"
+	imageName = "plots/primaryplots/nuflavorint.jpg";
+    console.log(val2);
+    console.log(imageName);
+    $('img').attr("src",imageName);
+});
+
+
+$('#revision').change(function () {
+    var val1 = parseInt($('#anita').val());
+    var val2 = parseInt($('#energy').val());
+    var val3 = parseInt($('#revision').val());
+    var val4 = $('#plot').val();
+    //imageName ="revision" + val3.toString() + "_anita" + val1.toString() +"_energy" + val2.toString() + val4 + ".pdf"
+	imageName="plots/primaryplots/currentint.jpg";
+    console.log(val2);
+    console.log(imageName);
+    $('img').attr("src",imageName);
+});
+
+
+
+$('#plot').change(function () {
+    var val1 = parseInt($('#anita').val());
+    var val2 = parseInt($('#energy').val());
+    var val3 = parseInt($('#revision').val());
+    var val4 = $('#plot').val();
+    //imageName ="revision" + val3.toString() + "_anita" + val1.toString() +"_energy" + val2.toString() + "_" + val4 + ".pdf"
+	imageName="plots/primaryplots/nuexit.jpg";
+    console.log(val2);
+    console.log(imageName);
+    $('img').attr("src",imageName);
+});
+
+
+});//]]>  
+
+</script>
+
+
 
 </head>
-
-	<body>
-
-		<div id="wrapper">
+<body>
+	<div id="wrapper">
 
 <?php include('includes/header.php'); ?>
 
@@ -31,13 +117,13 @@
 <h3>Compare</h3>
 
 <p>
-This is a placeholder for the comparison page. It is currently under development.
+This is a placeholder for the comparison page.
 </p>
 
 <p>
 Hopefully this page will eventually allow the user to select 
-specific AraSimQC plots from releases or trunks and compare 
-them side-by-side.  I don't know how to do this yet.
+specific IcemcQC plots from releases or trunks and compare 
+them side-by-side.  I dont know how to do this yet.
 </p>
 
 <p>
@@ -46,59 +132,59 @@ trunks, and plots listed actually exist yet!
 </p>
 
 <p>
-Select the first version you would like to compare:
-<select name="chooseVersion1">
-  <option value="">Select...</option>
-  <option value="rel_1_0">Release 1.0</option>
-  <option value="rel_1_1">Release 1.1</option>
-  <option value="trunk493">Trunk 493</option>
-  <option value="trunk493">Trunk 493</option>
-  <option value="trunk494">Trunk 494</option>
-  <option value="trunk495">Trunk 495</option>
-  <option value="trunk496">Trunk 496</option>
-</select>
+Please select the parameters for the first set of plots.
 </p>
+
+Select Anita Flight:<select id="anita">
+	<option>- - - - - - </option>
+	<option value="1">Anita 1 </option>
+	<option value="2">Anita 2 </option>
+	<option value="3">Anita 3 </option>
+</select>
+
+Select Energy: <select id="energy">
+	<option>- - - - - - </option>
+	<option value="18">10^18 </option>
+	<option value="19">10^19 </option>
+	<option value="20">10^20 </option>
+	<option value="21">10^21 </option>
+</select>
+
+Select Revision Number: <select id="revision">
+	<option>- - - - - - </option>
+	<option value="500">500 </option>
+	<option value="501">501 </option>
+	<option value="502">502 </option>
+	<option value="503">503 </option>
+	</select>
+
+Select Variable: <select id="plot">
+        <option>- - - - - - </option>
+	<option value="all">All variables! </option>
+	<option value="altitude_int">altitude_int </option>
+	<option value="chord_kgm2_bestcase">chord_kgm2_bestcase </option>
+    	<option value="currentint">current_int </option>
+</select>			   
 
 <p>
-Select the second version you would like to compare:
-<select name="chooseVersion2">
-  <option value="">Select...</option>
-  <option value="rel_1_0">Release 1.0</option>
-  <option value="rel_1_1">Release 1.1</option>
-  <option value="trunk493">Trunk 493</option>
-  <option value="trunk494">Trunk 494</option>
-  <option value="trunk495">Trunk 495</option>
-  <option value="trunk496">Trunk 496</option>
-</select>
+Please select the parameters for the second set of plots.
 </p>
 
-<label for='choosePlots[]'>Select the plots you would like to view (hold Ctrl to select multiple):</label><br>
-<select multiple="multiple" name="choosePlots[]">
-    <option value="allPlots">All plots!</option>
-    <option value="01">Plot 01</option>
-    <option value="02">Plot 02</option>
-    <option value="03">Plot 03</option>
-    <option value="04">Plot 04</option>
-    <option value="05">Plot 05</option>
-    <option value="06">Plot 06</option>
-</select>
 
-<br> <br> <br> <br>
-This will make the plots go
-<select name="Plot">
-    <option value="" selected="selected"></option>
-  <?php 
-    $plotdir = "plots/rel_1_0/trajP500.jpg";//your path
-    $dh  = opendir($plotdir);
-    while (false !== ($filename = readdir($dh))) {
-        $files[] = $filename;
-  echo "<option value='" . $filename . "'>".$filename."</option>";  }
-    sort($files);
- ?>
+<img src="plots/primaryplots/posnu.jpg" />
+<!--<object id="imageName" data="plots/primaryplots/posnu.pdf" type="application/pdf" width="900" height="300">
+ 
+  <p>It appears you don't have a PDF plugin for this browser.
+  No biggie... you can <a href="posnu.pdf">click here to
+  download the PDF file.</a></p>
+  
+</object>-->
 
-</select> 
+
+
 
 <?php
+
 //$ver1 = $_POST['chooseVersion1'];
 //    switch($ver1)
 //	$comp1="rel_1_0"
